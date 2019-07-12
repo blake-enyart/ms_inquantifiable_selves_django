32dec57016cb515ce2cfec144c1576444d26d5b3
Blake <blake.enyart@gmail.com>
2019-07-12 12:29:53 -0600

    Recipe service using edamam client module to query and save food/recipes to database

--- edamam_service/services/recipe_service.py
+++ edamam_service/services/recipe_service.py
@@ -1,11 +1,23 @@
 from .clients import edamam_client
+from ..models import Food, Recipe
 
 class RecipeService(object):
     def __init__(self, query):
         self.query = query
 
-    @cached_property
     def get_recipes(self):
         return edamam_client.get_json(self.query)['hits']
 
-    
+    def save_to_db(self):
+        recipes = self.get_recipes()
+        new_food = Food(name=self.query)
+        new_food.save()
+        for recipe in recipes:
+            recipe = recipe['recipe']
+            new_food.recipes.create(
+                image_url=recipe['image'],
+                ingredient_count=len(recipe['ingredients']),
+                name = recipe['label'],
+                url = recipe['url']
+            )
+        return None
